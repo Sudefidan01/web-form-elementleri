23d01dc6cb927db66e2307bdd89a73b5ec302ed9
sude <sudefidan@icloud.com>
2024-10-17 21:05:02 +0300

    web-form-elementleri

---
+++ 18112023_WebFormElementleri/18112023_WebFormElementleri/index.aspx
@@ -0,0 +1,85 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="_18112023_WebFormElementleri.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+       <h1> Form Elementleri</h1>
+        <h2>Textbox</h2>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server"  Text="Tıkla" OnClick="Button1_Click" />
+        <br />
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+        <%-- Checkbox --%>
+        <div>
+            <hr />
+            <h2>Checkbox</h2>
+            <hr />
+            <p>Kullandığınız programlama dillerini seçiniz</p>
+            <asp:CheckBox ID="CheckBox1" runat="server" Text="C#"/>
+            <asp:CheckBox ID="CheckBox2" runat="server" Text="Pyhton"/>
+            <asp:CheckBox ID="CheckBox3" runat="server" Text="Java"/>
+            <asp:CheckBox ID="CheckBox4" runat="server" Text="Kotlin"/>
+            <asp:CheckBox ID="CheckBox5" runat="server" Text="Javascript"/>
+            <br />
+            <asp:Button ID="btnCheckbox" runat="server" Text="Oku" OnClick="btnCheckbox_Click" />
+            <br />
+            <asp:Label ID="lblCheckbox" runat="server" ></asp:Label>
+        </div>
+        <%-- RadioButton --%>
+        <div>
+            <h2>RadioButton</h2>
+            <hr />
+            <p>Cinsiyetinizi Seçiniz</p>
+            <asp:RadioButton ID="RadioButton1" runat="server" GroupName="gender" Text="Erkek"/>
+            <asp:RadioButton ID="RadioButton2" runat="server" Text="Kadın" GroupName="gender" />
+            <asp:RadioButton ID="RadioButton3" runat="server" Text="Belirtmek İstemiyorum" GroupName="gender" />
+            <br />
+            <asp:Button ID="btnRadioButton" runat="server" Text="Oku" OnClick="btnRadioButton_Click" />
+            <br />
+            <asp:Label ID="lblRadioButton" runat="server" ></asp:Label>
+        </div>
+        <%-- DropdownList --%>
+
+        <div>
+            <h2>DropdownList</h2>
+            <hr />
+            <asp:DropDownList ID="ddlSehirler" runat="server">
+            <asp:ListItem value="34">İstanbul</asp:ListItem>
+            <asp:ListItem value="06">Ankara</asp:ListItem>
+            <asp:ListItem value="35">İzmir</asp:ListItem>
+            <asp:ListItem value="54">Sakarya</asp:ListItem>
+            <asp:ListItem value="01">Adana</asp:ListItem>
+
+            </asp:DropDownList>
+            <hr />
+            <asp:Button ID="btnSehirOku" runat="server" Text="Oku" OnClick="btnSehirOku_Click" />
+            <asp:Button ID="btnYeniDeger" runat="server" Text="Şehirleri Değiştir" OnClick="btnYeniDeger_Click" />
+            <br />
+            <asp:Label ID="lblSehir" runat="server" ></asp:Label>
+        </div>
+        <%-- Image --%>
+        <div>
+            <h2>Image</h2>
+            <hr />
+            <asp:Image ID="Image1" runat="server" Width="300px" ImageUrl="~/images/armut.jpg" />
+            <br />
+            <asp:Button ID="btnImage" runat="server" Text="Değiştir" OnClick="btnImage_Click" />
+        </div>
+        <%-- FileUpload --%>
+        <div>
+            <h2>FileUpload</h2>
+            <hr />
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <br />
+            <asp:Button ID="btnUpload" runat="server" Text="Dosya Yükle" OnClick="btnUpload_Click" />
+            <asp:Label ID="lblUpload" runat="server" ></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 18112023_WebFormElementleri/18112023_WebFormElementleri/index.aspx.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _18112023_WebFormElementleri
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string metin = TextBox1.Text;
+            if (string.IsNullOrEmpty(metin))
+            {
+                Label1.Text = "Metin alanı boş bırakılamaz";
+                Label1.ForeColor = Color.Red;
+            }
+            else
+            {
+                Label1.Text = metin;
+                TextBox1.Text = string.Empty;
+                Label1.ForeColor = Color.Green;
+            }
+        }
+
+        protected void btnCheckbox_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+            if (CheckBox1.Checked)
+            {
+                list.Add(CheckBox1.Text);
+            }
+            if (CheckBox2.Checked)
+            {
+                list.Add(CheckBox2.Text);
+            }
+            if (CheckBox3.Checked)
+            {
+                list.Add (CheckBox3.Text);
+            }
+            if (CheckBox4.Checked)
+            {
+                list.Add(CheckBox4.Text);
+            }
+            if (CheckBox5.Checked) 
+            {
+                list.Add(CheckBox5.Text);
+            }
+            string secilenler = string.Empty;
+            if (list.Any())
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    secilenler += (i==0 ?"":",")+list[i];
+                }
+                lblCheckbox.Text = "Seçilen Değeler = " + secilenler;
+                lblCheckbox.ForeColor = Color.Green;
+            }
+            else
+            {
+                lblCheckbox.Text = "En az bir seçim yapmalısınız";
+                lblCheckbox.ForeColor = Color.Red;  
+            }
+        }
+
+        protected void btnRadioButton_Click(object sender, EventArgs e)
+        {
+            if (RadioButton1.Checked)
+            {
+                lblRadioButton.Text = RadioButton1.Text;
+            }
+            else if (RadioButton2.Checked) 
+            {
+                lblRadioButton.Text = RadioButton2.Text;
+            }
+            else if(RadioButton3.Checked)
+            {
+                lblRadioButton.Text = RadioButton3.Text;
+            }
+            else
+            {
+                lblRadioButton.Text = "Seçim Yapmalısınız ";
+            }
+        }
+
+        protected void btnSehirOku_Click(object sender, EventArgs e)
+        {
+            lblSehir.Text = string.Format("Text: {0} , Value: {1}",ddlSehirler.SelectedItem.Text,ddlSehirler.SelectedItem.Value);
+        }
+
+        protected void btnYeniDeger_Click(object sender, EventArgs e)
+        {
+            ddlSehirler.Items.Clear();
+            ddlSehirler.Items.Add(new ListItem("Kastamonu","37"));
+            ddlSehirler.Items.Add(new ListItem("Rize","53"));
+            ddlSehirler.Items.Add(new ListItem("Sivas","58"));
+            ddlSehirler.Items.Add(new ListItem("Çanakkale","17"));
+            ddlSehirler.Items.Add(new ListItem("Bursa","16"));
+        }
+
+        protected void btnImage_Click(object sender, EventArgs e)
+        {
+            if (Image1.ImageUrl== "~/images/armut.jpg")
+            {
+                Image1.ImageUrl = "~/images/cilek.jpg";
+            }
+            else if (Image1.ImageUrl == "~/images/cilek.jpg")
+            {
+                Image1.ImageUrl = "~/images/elma.jpg";
+            }
+
+            else if (Image1.ImageUrl== "~/images/elma.jpg")
+            {
+                Image1.ImageUrl = "~/images/karpuz.jpg";
+            }
+            else if (Image1.ImageUrl== "~/images/karpuz.jpg")
+            {
+                Image1.ImageUrl = "~/images/kivi.jpg";
+            }
+            else
+            {
+                Image1.ImageUrl = "~/images/armut.jpg";
+            }
+
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile) //File upload ile bir dosya seçildi mi
+            {
+                if (FileUpload1.PostedFile.ContentLength>0)
+                {
+                    if (FileUpload1.FileName.Contains(".jpg") || FileUpload1.FileName.Contains(".jpeg") ||
+                        FileUpload1.FileName.Contains(".png"))
+                    {
+                        string fileName = Guid.NewGuid().ToString() + "_" + FileUpload1.ToString();
+                        FileUpload1.SaveAs(Server.MapPath("~/images/"+fileName));
+                        lblUpload.Text = "Dosya yükleme işlemi başarılı";
+                    }
+                    else
+                    {
+                        lblUpload.Text = "Lütfen .png .jpg .jpeg uzantısına sahip resimleri ekleyenizi";
+                    }
+
+                }
+                else
+                {
+
+                    lblUpload.Text = "Lütfen geçerli bir dosya seçiniz";
+                }
+            }
+            else
+            {
+                lblUpload.Text = "Lütfen dosya seçimi yapınız";
+            }
+        }
+    }
+}
